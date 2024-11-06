88e5daea99f8fdd1c88a6b1549a9103e436e6a49
AsterX2 <karim.jerjoub@ensea.fr>
2024-11-06 17:41:10 +0100

    TP1 avancement
    Problème ARR à corriger
    ET visu sur double channel à activer
    reste ok

---
+++ NUCLEO-G474RET6-Inverter_Pinout/Core/Src/mylibs/shell.c
@@ -0,0 +1,118 @@
+/*
+ * shell.c
+ *
+ *  Created on: Oct 1, 2023
+ *      Author: nicolas
+ */
+#include "usart.h"
+#include "mylibs/shell.h"
+#include "tim.h"
+
+//#include "../tim.c"
+
+#include <stdio.h>
+#include <string.h>
+
+uint8_t prompt[]="user@Nucleo-STM32G474RET6>>";
+uint8_t started[]=
+		"\r\n*-----------------------------*"
+		"\r\n| Welcome on Nucleo-STM32G474 |"
+		"\r\n*-----------------------------*"
+		"\r\n";
+uint8_t newline[]="\r\n";
+uint8_t backspace[]="\b \b";
+uint8_t cmdNotFound[]="Command not found\r\n";
+uint8_t brian[]="Brian is in the kitchen\r\n";
+uint8_t uartRxReceived;
+uint8_t uartRxBuffer[UART_RX_BUFFER_SIZE];
+uint8_t uartTxBuffer[UART_TX_BUFFER_SIZE];
+
+//extern TIM_HandleTypeDef htim1;
+
+char	 	cmdBuffer[CMD_BUFFER_SIZE];
+int 		idx_cmd;
+char* 		argv[MAX_ARGS];
+int		 	argc = 0;
+char*		token;
+int 		newCmdReady = 0;
+
+void Shell_Init(void){
+	memset(argv, NULL, MAX_ARGS*sizeof(char*));
+	memset(cmdBuffer, NULL, CMD_BUFFER_SIZE*sizeof(char));
+	memset(uartRxBuffer, NULL, UART_RX_BUFFER_SIZE*sizeof(char));
+	memset(uartTxBuffer, NULL, UART_TX_BUFFER_SIZE*sizeof(char));
+
+	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+	HAL_UART_Transmit(&huart2, started, strlen((char *)started), HAL_MAX_DELAY);
+	HAL_UART_Transmit(&huart2, prompt, strlen((char *)prompt), HAL_MAX_DELAY);
+}
+
+void Shell_Loop(void){
+	if(uartRxReceived){
+		switch(uartRxBuffer[0]){
+		case ASCII_CR: // Nouvelle ligne, instruction à traiter
+			HAL_UART_Transmit(&huart2, newline, sizeof(newline), HAL_MAX_DELAY);
+			cmdBuffer[idx_cmd] = '\0';
+			argc = 0;
+			token = strtok(cmdBuffer, " ");
+			while(token!=NULL){
+				argv[argc++] = token;
+				token = strtok(NULL, " ");
+			}
+			idx_cmd = 0;
+			newCmdReady = 1;
+			break;
+		case ASCII_BACK: // Suppression du dernier caractère
+			cmdBuffer[idx_cmd--] = '\0';
+			HAL_UART_Transmit(&huart2, backspace, sizeof(backspace), HAL_MAX_DELAY);
+			break;
+
+		default: // Nouveau caractère
+			cmdBuffer[idx_cmd++] = uartRxBuffer[0];
+			HAL_UART_Transmit(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE, HAL_MAX_DELAY);
+		}
+		uartRxReceived = 0;
+	}
+
+	if(newCmdReady){
+		if(strcmp(argv[0],"WhereisBrian?")==0){
+			HAL_UART_Transmit(&huart2, brian, sizeof(brian), HAL_MAX_DELAY);
+		}
+		else if(strcmp(argv[0],"help")==0){
+			int uartTxStringLength = snprintf((char *)uartTxBuffer, UART_TX_BUFFER_SIZE, "Print all available functions here\r\n");
+			HAL_UART_Transmit(&huart2, uartTxBuffer, uartTxStringLength, HAL_MAX_DELAY);
+		}
+		else{
+			HAL_UART_Transmit(&huart2, cmdNotFound, sizeof(cmdNotFound), HAL_MAX_DELAY);
+		}
+		HAL_UART_Transmit(&huart2, prompt, sizeof(prompt), HAL_MAX_DELAY);
+		newCmdReady = 0;
+	}
+	else if(strcmp(argv[0], "speed") == 0) {
+	    if(argc > 1) {  // Assurez-vous qu'il y a un argument après "speed"
+	        int speedValue = atoi(argv[1]);  // Convertir l'argument en entier
+	        // Faites quelque chose avec speed_value, comme l'afficher ou le traiter : on change le duty cycle
+	        if (speedValue < 0) speedValue = 0;
+	           if (speedValue > 945) speedValue = 900;
+
+	           //uint32_t dutyCycle = 614;  // Exemple de valeur de rapport cyclique (60% si la période est 1023)
+
+	           __HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, speedValue);//alpha PWM channel 1 changé (U_High).Le complémentaire se change de lui-même par définition
+	           //Il faudra changer le rapport cyclique de l'autre channel (channel 2)
+
+	           //__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, speedValue); //V_High
+
+	           //uint8_t set_speed="Speed set to"+speedValue;
+	           //HAL_UART_Transmit(&huart2, (uint8_t *)" Speed set to %d \n\r",speedValue, 30, HAL_MAX_DELAY);
+	           HAL_UART_Transmit(&huart2, speedValue, 30, HAL_MAX_DELAY);
+
+	    } else {
+	        HAL_UART_Transmit(&huart2, (uint8_t *)"Please provide a speed value\r\n", 30, HAL_MAX_DELAY);
+	    }
+	}
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef * huart){
+	uartRxReceived = 1;
+	HAL_UART_Receive_IT(&huart2, uartRxBuffer, UART_RX_BUFFER_SIZE);
+}
